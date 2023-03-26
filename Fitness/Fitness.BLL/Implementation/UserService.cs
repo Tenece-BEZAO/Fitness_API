f97581bc3d1247101ede6c66e991beb89bdb16c4
Chelseaquiin <uzonweze@gmail.com>
2023-03-27 00:14:30 +0100

    Feat: Created the User Entity class

---
+++ Fitness/Fitness.BLL/Implementation/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Fitness.BLL.Interface;
+using Fitness.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BLL.Implementation
+{
+    public class UserService : IUserService
+    {
+        private readonly FitnessDbContext _context;
+
+        public UserService(IUserService context)
+        {
+            _context = context;
+        }
+        public void CreateAUser()
+        {
+
+        }
+        public void UpdateAUser()
+        {
+
+        }
+        public void DeleteAUser()
+        {
+
+        }
+        public void GetAUser()
+        {
+
+        }
+    }
+}
