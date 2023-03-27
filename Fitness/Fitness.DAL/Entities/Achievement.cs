6f7e46261abab0c3594c7362dbe0a9f6b98e8358
Lynda nneka <anaedobelyn001@gmail.com>
2023-03-27 02:46:00 +0100

    Added enums folder and entities for fitness tracking

---
+++ Fitness/Fitness.DAL/Entities/Achievement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Fitness.DAL.Entities
+{
+    public class Achievement : BaseEntity
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
+
---
+++ Fitness/Fitness.DAL/Entities/WorkOutExercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Fitness.DAL.Entities
+{
+    public class WorkOutExercise : BaseEntity
+    {
+        public int WorkOutId { get; set; }
+        public int ExerciseId { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+    }
+}
+
