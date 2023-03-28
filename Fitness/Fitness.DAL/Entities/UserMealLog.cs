f060c8d7fd2cd6692918d21073ae4509d1317969
Acha <99018977+acha-r@users.noreply.github.com>
2023-03-29 00:08:40 +0100

    Added nutrition models (#6)
    * Implemented API configuration
    
    * Implemented API configuration
    
    * Added nutrion models

---
+++ Fitness/Fitness.DAL/Entities/UserMealLog.cs
@@ -0,0 +1,19 @@
+﻿using Fitness.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.DAL.Entities
+{
+    public class UserMealLog : MealPlanLog
+    {
+       
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+    }
+
+
+}
