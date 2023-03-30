c53724ac419a59ff8aa1a8e1834c4f527809b72a
Lynda nneka <86114880+lynda-nneka@users.noreply.github.com>
2023-03-30 16:41:10 +0100

    Removed the date property from the achievement entity and added it to the userAchievement (#9)

--- Fitness/Fitness.DAL/Entities/Achievement.cs
+++ Fitness/Fitness.DAL/Entities/Achievement.cs
@@ -6,7 +6,6 @@ namespace Fitness.DAL.Entities
     public class Achievement : BaseEntity
     {
         public string Name { get; set; }
-        public DateTime Date { get; set; }
         public string Description { get; set; }
 
         public List<FitFamer> FitFamers { get; set; }
